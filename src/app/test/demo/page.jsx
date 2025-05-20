5d33e666861d4d9d3706c4148af49c017aba63c0
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-20 23:24:38 +0530

    started

--- src/app/test/demo/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function DemoPage(){
-    return (
-        <>
-        <h1 className="text-center text-2xl text-white">Welcome buddy ....!</h1>
-        </>
-    )
-}
--- src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function test(){
-    return(
-        <>
-        <h1 className="text-orange-400 text-3xl p-4 w-full mt-5 text-center">Hey Buddy...!</h1>
-        </>
-    )
-}
